d1c85ae5f8c4a3f301236d8f96b55ed8790143b1
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:33:59 -0400

    first commit

---
+++ myclass.cpp
@@ -0,0 +1,17 @@
+#include "myclass.h"
+
+MyClass::MyClass(QObject *parent, QString name) : QObject(parent)
+{
+    setObjectName(name);
+    qInfo() << this << "Constructed";
+}
+
+MyClass::~MyClass()
+{
+    qInfo() << this << "Destroyed";
+}
+
+void MyClass::test()
+{
+    qInfo() << this << "test called";
+}
